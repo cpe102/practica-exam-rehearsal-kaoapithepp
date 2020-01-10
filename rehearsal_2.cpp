1b5e7ff7a45bb924b69a962181391368587ce283
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-10 14:55:52 +0700

    first attemp

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    float sum = 0,x;
+
+    
+    while (x != 0) {
+        cout << "Enter x: ";
+        cin >> x;
+        sum = sum + x;
+    }
+
+    cout << "sum = " << sum << endl;
+
+    return 0;
+}
